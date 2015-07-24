77bf09daae72141bae5e780798befa67f6832d55
Timothy Baldock <timothy.baldock@red-gate.com>
2015-07-24 17:52:14 +0100

    Add back sample data

---
+++ GoOutside/SampleData/PopUpViewModelSample.cs
@@ -0,0 +1,15 @@
+﻿namespace GoOutside.SampleData
+{
+    class PopUpViewModelSample
+    {
+        public bool Visible { get; set; }
+
+        public double Top { get; set; }
+
+        public PopUpViewModelSample()
+        {
+            Visible = true;
+            Top = 200;
+        }
+    }
+}
