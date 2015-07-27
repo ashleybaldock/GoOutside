219b5370c5b0e04221a8d30c2332398ab149c7db
Timothy Baldock <timothy.baldock@red-gate.com>
2015-07-27 18:57:28 +0100

    Display text on tomato

--- GoOutside/ViewModels/PomoViewModel.cs
+++ GoOutside/ViewModels/PomoViewModel.cs
@@ -1,7 +1,12 @@
-﻿namespace GoOutside.ViewModels
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace GoOutside.ViewModels
 {
-    public class PomoViewModel : IPomoViewModel
+    public class PomoViewModel : INotifyPropertyChanged, IPomoViewModel
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public PomoViewModel()
         {
             Visible = true;
@@ -12,5 +17,56 @@
         public double Width { get { return 200; } }
         public bool Visible { get; set; }
         public string TimerText { get; set; }
+
+        public ICommand MouseEnterCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () =>
+                    {
+                        TimerText = "MouseOver";
+                        NotifyPropertyChanged("TimerText");
+                    }
+                };
+            }
+        }
+
+        public ICommand MouseLeaveCommand
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () =>
+                    {
+                        TimerText = "25:00";
+                        NotifyPropertyChanged("TimerText");
+                    }
+                };
+            }
+        }
+
+        public ICommand StopPomo
+        {
+            get
+            {
+                return new DelegateCommand
+                {
+                    CommandAction = () =>
+                    {
+                    }
+                };
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
